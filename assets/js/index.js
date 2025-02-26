12f8857b9797f40e6ae043f7ee6f34ba0e1b23b2
Raihan <aboraihan14@gmail.com>
2025-02-27 01:12:46 +0600

    navbar added

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".mobile-menu");
+
+    // Hamburger Button Click Functionality
+    hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+
+    // Auto Hide Mobile Menu when Screen Resizes Above 900px
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 900) {
+            navMenu.classList.remove("active");
+        }
+    });
+});
